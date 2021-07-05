8a4dd53c9d1c686e08016973f7ed3ae9e75e65ba
qq1468276607 <74702787+qq1468276607@users.noreply.github.com>
2021-07-05 21:36:37 +0800

    Add files via upload

---
+++ .umirc.js
@@ -0,0 +1,31 @@
+
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: false,
+      title: 'umi-demo',
+      dll: false,
+      
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+  proxy: {
+    '/api': {
+      target: 'http://192.168.105.233:9666',
+      changeOrigin: true,
+    },
+  },
+}
